9bc6ee346ab256175016f53472af45cd8491280d
Eka Risyana <eka.risyana@mekari.com>
2023-05-05 09:50:27 +0700

    handle endpoint with multiple URL param

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	router.GET("/", handleRoot)
 	router.GET("/todo/:id", handleGetTodoById)
 	router.GET("/todos", handleUrlQuery)
+	router.GET("/todos/:year/:month", handleMultiUrlParam)
 
 	router.Run(":7878")
 }
@@ -34,5 +35,14 @@ func handleUrlQuery(c *gin.Context) {
 	title := c.Query("title")
 	c.JSON(http.StatusOK, gin.H{
 		"title": title,
+	})
+}
+
+func handleMultiUrlParam(c *gin.Context) {
+	year := c.Param("year")
+	month := c.Param("month")
+	c.JSON(http.StatusOK, gin.H{
+		"year":  year,
+		"month": month,
 	})
 }
