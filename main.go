8f25769af94899a89a3d52abca5ed80b22778e4b
Eka Risyana <eka.risyana@mekari.com>
2023-05-05 09:41:47 +0700

    handle endpoint with URL param (dynamic URL)

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", handleRoot)
+	router.GET("/todo/:id", handleGetTodoById)
 
 	router.Run(":7878")
 }
@@ -18,5 +19,12 @@ func handleRoot(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"name":    "Eka",
 		"address": "Bandung x",
+	})
+}
+
+func handleGetTodoById(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
 	})
 }
