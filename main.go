0b463a854a0bcbdf7a9a2619b69946c42fdd414e
Eka Risyana <eka.risyana@mekari.com>
2023-05-20 13:41:14 +0700

    connect to mysql db using gorm

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"belajar-golang-api-revisit-1/handler"
+	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/belajar-golang-api-revisit-1?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Error DB connection")
+	}
+	fmt.Println("Succes DB connection")
+
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
