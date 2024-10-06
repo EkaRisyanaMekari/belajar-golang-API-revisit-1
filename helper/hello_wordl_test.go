2269ffc431e530d14ffe7da15563af868a7251fd
Eka Risyana <eka.risyana@mekari.com>
2024-10-06 14:46:44 +0700

    using assert to test

--- helper/hello_wordl_test.go
+++ helper/hello_wordl_test.go
@@ -1,10 +1,19 @@
 package helper
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("eka")
-	if result != "hello eka x" {
+	if result != "hello eka" {
 		t.Fatal("tidak sesuai")
 	}
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("eka")
+	assert.Equal(t, "hello eka", result)
 }
